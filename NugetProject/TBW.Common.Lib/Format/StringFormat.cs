cef4176db2b14b1f25121d95f41938ae9f4cd3fd
田博文 <1471758682@qq.com>
2023-02-24 11:03:14 +0800

    Update function code
    New Formatted version information and README.md brief file.

---
+++ NugetProject/TBW.Common.Lib/Format/StringFormat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBW.Common.Lib.Format
+{
+    public class StringFormat
+    {
+        public static string FormatVersion_V1(string major, string minor, string build, string revision)
+        {
+            return string.Format("{0}.{1}.{2}.{3}", major, minor, build, revision);
+        }
+
+        public static string FormatVerion_V2(string major, string minor, string build, string revision) =>
+            $"{major}.{minor}.{build}.{revision}";
+
+        public static string FormatVerion_V3(string major, string minor, string build, string revision)
+        {
+            // //in .NET 10 C# 6
+            //var handler = new DefaultInterpolatedStringHandler(literalLength: 3, formattedCount: 4);
+            //handler.AppendFormatted(major);
+            //handler.AppendFormatted(".");
+            //handler.AppendFormatted(minor);
+            //handler.AppendFormatted(".");
+            //handler.AppendFormatted(build);
+            //handler.AppendFormatted(".");
+            //handler.AppendFormatted(revision);
+            //return handler.ToStringAndClear();
+
+            var array = new object[4];
+            array[0] = major;
+            array[1] = minor;
+            array[2] = build;
+            array[3] = revision;
+            return string.Format("{0}.{1}.{2}.{3}",array);
+        }
+    }
+}
---
+++ NugetProject/TBW.Common.Lib/README.md
@@ -0,0 +1,18 @@
+功能目录:
+
+- Algorithm(算法)
+  - KMP - 文本中快速查找某字符串模式的算法实现
+- Extension(扩展)
+  - StringExtension(字符串扩展方法)
+    -  StringEx - 包含字符串空值判断，强制转换等。
+- Format(格式化)
+  - StringFormat - Semantic Versioning(语义化版本)格式化等。
+- Helper(帮助)
+  - FTP(文件传输协议)
+    - FTPHelper - FTP基本操作，连接，下载，上传文件等。
+- Log(日志)
+  - StaticFile(静态文件)
+    - StaticFileLogger - 按年月日时间记录日志到txt文件。
+- Sequence(序列)
+  - FibonacciSequence -  斐波那契数列
+
